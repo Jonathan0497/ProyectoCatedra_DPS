77a252efb157951e7d1b0a0311d39976b19f3fad
Carislost <135508493+Carislost@users.noreply.github.com>
2024-05-06 02:38:40 -0600

    Se creo el crud de marca como una prueba
    Co-Authored-By: Jonathan0497 <64447212+Jonathan0497@users.noreply.github.com>

--- api/models/marca.php
+++ api/models/marca.php
@@ -4,8 +4,6 @@ class Marca extends Validator
 {
     private $id = null;
     private $nombre = null;
-    private $image = null;
-    private $ruta = '../images/marcas/';
 
     public function setId($value)
     {
@@ -37,54 +35,33 @@ class Marca extends Validator
         return $this->nombre;
     }
 
-    public function setImagen($file)
-    {
-        if ($this->validateImageFile($file, 500, 500)) {
-            $this->image = $this->getFileName();
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public function getImagen()
-    {
-        return $this->image;
-    }
-
-    public function getRuta()
-    {
-        return $this->ruta;
-    }
 
     public function createRow()
     {
-        $sql = 'INSERT INTO marca(nombre_marca, imagen_marca) VALUES(?, ?)';
-        $params = array($this->nombre, $this->image);
+        $sql = 'INSERT INTO marca(nombre_marca) VALUES(?)';
+        $params = array($this->nombre);
         return Database::executeRow($sql, $params);
     }
 
     public function readAll()
     {
-        $sql = 'SELECT id_marca, nombre_marca, imagen_marca FROM marca ORDER BY nombre_marca';
+        $sql = 'SELECT id_marca, nombre_marca FROM marca ORDER BY nombre_marca';
         $params = null;
         return Database::getRows($sql, $params);
     }
 
     public function readOne()
     {
-        $sql = 'SELECT id_marca, nombre_marca, imagen_marca FROM marca WHERE id_marca = ?';
+        $sql = 'SELECT id_marca, nombre_marca FROM marca WHERE id_marca = ?';
         $params = array($this->id);
         return Database::getRow($sql, $params);
     }
 
-    public function updateRow($current_image)
+    public function updateRow()
     {
-        // Se verifica si existe una nueva imagen para borrar la actual, de lo contrario se mantiene la actual.
-        ($this->image) ? $this->deleteFile($this->getRuta(), $current_image) : $this->image = $current_image;
 
-        $sql = 'UPDATE marca SET nombre_marca = ?, imagen_marca = ? WHERE id_marca = ?';
-        $params = array($this->nombre, $this->image, $this->id);
+        $sql = 'UPDATE marca SET nombre_marca = ? WHERE id_marca = ?';
+        $params = array($this->nombre, $this->id);
         return Database::executeRow($sql, $params);
     }
 
